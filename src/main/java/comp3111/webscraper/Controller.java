54e29dcd376e9536480e51099b85efd35b60bad6
Chloe Kim <chloeminkyung@gmail.com>
2018-10-11 10:45:54 +0800

    put Hi in summary when actionSearch()

--- src/main/java/comp3111/webscraper/Controller.java
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+//import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Hyperlink;
 import java.util.List;
@@ -43,20 +43,20 @@ public class Controller {
     @FXML
     private TextArea textAreaConsole;
     
-    @FXML
-    private TableView<Item> tableView;
-    
-    @FXML
-    private TableColumn<Item, String> tTitle;
-    
-    @FXML
-    private TableColumn<Item, String> tPrice;
-    
-    @FXML
-    private TableColumn<Item, String> tURL;
-    
-    @FXML
-    private TableColumn<Item, String> tDate;
+//    @FXML
+//    private TableView<Item> tableView;
+//    
+//    @FXML
+//    private TableColumn<Item, String> tTitle;
+//    
+//    @FXML
+//    private TableColumn<Item, String> tPrice;
+//    
+//    @FXML
+//    private TableColumn<Item, String> tURL;
+//    
+//    @FXML
+//    private TableColumn<Item, String> tDate;
     
     private WebScraper scraper;
     
@@ -83,28 +83,25 @@ public class Controller {
     	System.out.println("actionSearch: " + textFieldKeyword.getText());
     	List<Item> result = scraper.scrape(textFieldKeyword.getText());
     	String output = "";
-    	List<Item> refined_result;
-    	for (Item item : result) {
-    		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
-//    		tTitle.setCellValueFactory(new PropertyValueFactory<Item, String>(item.getTitle()));
-//    		tURL.setCellValueFactory(new PropertyValueFactory<Item, String>(item.getUrl()));
-    	}
-    	textAreaConsole.setText(output);
-    	tableView.getItems().setAll(result);
-    }
-    
-    @FXML
-    private void refineSearch() {
-    	System.out.println("refineSearch: " + textFieldKeyword.getText());
-    	List<Item> result = scraper.scrape(textFieldKeyword.getText());
-    	String output = "";
     	for (Item item : result) {
     		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
     	}
     	textAreaConsole.setText(output);
+    	////////////////////////////////
+    	labelCount.setText("Hi");
     }
     
-    
+//    @FXML
+//    private void refineSearch() {
+//    	System.out.println("refineSearch: " + textFieldKeyword.getText());
+//    	List<Item> result = scraper.scrape(textFieldKeyword.getText());
+//		List<Item> refined_result;
+//    	String output = "";
+//    	for (Item item : result) {
+//    		output += item.getTitle() + "\t" + item.getPrice() + "\t" + item.getUrl() + "\n";
+//    	}
+//    	textAreaConsole.setText(output);
+//    }
     
     /**
      * Called when the new button is pressed. Very dummy action - print something in the command prompt.
